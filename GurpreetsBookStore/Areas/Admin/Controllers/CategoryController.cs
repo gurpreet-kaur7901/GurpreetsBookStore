2d5ff4a81db3092df3bb38b0bd665a9fa0e690fb
W0824816 <W0824816@myscc.ca>
2023-11-14 10:57:05 -0500

    commit

---
+++ GurpreetsBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using GurpreetsBooks.DataAccess.Repository.IRepository;
+using GurpreetsBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GurpreetsBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+       
+
+        //api calls
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+
+       
+    }
+}
