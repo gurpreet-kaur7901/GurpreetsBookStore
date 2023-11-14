da7ae244aa092e0f3082635d778ec39543648c90
W0824816 <W0824816@myscc.ca>
2023-11-14 09:46:31 -0500

    completed 1

--- GurpreetsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ GurpreetsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace GurpreetsBookStore.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- GurpreetsBookStore/Startup.cs
+++ GurpreetsBookStore/Startup.cs
@@ -64,7 +64,7 @@ namespace GurpreetsBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
