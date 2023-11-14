e8f51436fd07e49857ffa7b390983c3ba5766d3a
W0824816 <W0824816@myscc.ca>
2023-11-14 09:54:01 -0500

    Complete 2.1(database)

--- GurpreetsBooks.DataAccess/Data/ApplicationDbContext.cs
+++ GurpreetsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GurpreetsBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace GurpreetsBookStore.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
