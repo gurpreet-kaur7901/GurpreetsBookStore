647f035eb2ac120b90fb4e6aab82ff93dd73c937
W0824816 <W0824816@myscc.ca>
2023-11-14 11:18:32 -0500

    commit

--- GurpreetsBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ GurpreetsBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -22,8 +22,44 @@ namespace GurpreetsBookStore.Areas.Admin.Controllers
             return View();
         }
 
-        
-       
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
 
         //api calls
         #region API CALLS
@@ -34,7 +70,19 @@ namespace GurpreetsBookStore.Areas.Admin.Controllers
             return Json(new { data = allObj });
         }
         #endregion
-
-       
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful" });
+        }
     }
 }
+       
+    
--- GurpreetsBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ GurpreetsBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,5 +10,7 @@ namespace GurpreetsBooks.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         ISP_Call SP_Call { get; }
+
+        void Save();
     }
 }
