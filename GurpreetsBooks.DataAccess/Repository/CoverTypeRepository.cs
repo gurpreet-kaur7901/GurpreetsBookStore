b050b2a2dce157999802d7c5a716ee93ffa13720
W0824816 <W0824816@myscc.ca>
2023-11-20 19:38:58 -0500

    add covertype.cs, covertyperepository, icovertyperepository, update dbcontext

---
+++ GurpreetsBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using GurpreetsBooks.DataAccess.Repository.IRepository;
+using GurpreetsBooks.Models;
+using GurpreetsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GurpreetsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType covertype)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == covertype.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = covertype.Name;
+                // _db.SaveChanges();
+            }
+        }
+    }
+}
