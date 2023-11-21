d34a35667ea4c13da38e32959a0370f29190d7d6
W0824816 <W0824816@myscc.ca>
2023-11-20 19:55:16 -0500

    covertyperepo in unitofwork

--- GurpreetsBooks.DataAccess/Repository/UnitOfWork.cs
+++ GurpreetsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -20,10 +20,13 @@ namespace GurpreetsBooks.DataAccess.Repository
 
             Category = new CategoryRepository(_db);
 
+            CoverType = new CoverTypeRepository(_db);
+
             SP_Call = new SP_Call(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
 
         public ISP_Call SP_Call { get; private set; }
 
